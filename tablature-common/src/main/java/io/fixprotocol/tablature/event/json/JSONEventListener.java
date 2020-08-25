ff0576bb8438248e7f8ffc846120ff94b1ca3aca
Don Mendelson <donmendelson@gmail.com>
2020-08-25 14:11:19 -0500

    Provide errors and warnings as machine readable form plus logs #35

--- tablature-common/src/main/java/io/fixprotocol/tablature/event/json/JSONEventListener.java
+++ tablature-common/src/main/java/io/fixprotocol/tablature/event/json/JSONEventListener.java
@@ -2,30 +2,35 @@ package io.fixprotocol.tablature.event.json;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
 import io.fixprotocol.tablature.event.Event;
 import io.fixprotocol.tablature.event.EventListener;
 
 public class JSONEventListener implements EventListener, AutoCloseable {
 
-  private final OutputStream outputStream;
-  private final ObjectMapper mapper = new ObjectMapper();
-
+  private final JsonGenerator generator;
 
   public JSONEventListener(OutputStream outputStream) throws IOException {
-    this.outputStream = outputStream;
+    JsonFactory factory = new JsonFactory();
+    generator = factory.createGenerator(outputStream);
+    generator.writeStartObject();
+    generator.writeArrayFieldStart("events");
   }
 
   @Override
   public void close() throws Exception {
-    outputStream.close();
+    generator.close();
   }
 
   @Override
   public void event(Event event) {
     try {
-      outputStream.write(mapper.writeValueAsBytes(event));
-    } catch (IOException e) {
+      generator.writeStartObject();
+      generator.writeObjectField("severity", event.getSeverity().name());
+      generator.writeObjectField("message", event.getMessage());
+      generator.writeEndObject();
+     } catch (IOException e) {
       throw new RuntimeException(e);
     }
   }
--- tablature-common/src/test/java/io/fixprotocol/tablature/event/json/JSONEventListenerTest.java
+++ tablature-common/src/test/java/io/fixprotocol/tablature/event/json/JSONEventListenerTest.java
@@ -17,7 +17,14 @@ class JSONEventListenerTest {
 
   private JSONEventListener listener;
   private ByteArrayOutputStream outputStream;
+  private Event event1;
+  private Event event2;
   
+  JSONEventListenerTest() {
+    event1 = new Event(WARN, "Warning message");
+    event2 = new Event(ERROR, "Error message {0}", 23);
+  }
+
   @BeforeEach
   void setUp() throws Exception {
     outputStream = new ByteArrayOutputStream(4096);
@@ -26,19 +33,26 @@ class JSONEventListenerTest {
 
   @Test
   void message() throws Exception {
-    Event event1 = new Event(ERROR, "Error message");
     listener.event(event1);
     listener.close();
-    assertEquals("{\"severity\":\"ERROR\",\"message\":\"Error message\"}", 
+    assertEquals("{\"events\":[{\"severity\":\"WARN\",\"message\":\"Warning message\"}]}", 
         outputStream.toString());
   }
   
   @Test
   void withArguments() throws Exception {
-    Event event1 = new Event(ERROR, "Error message {0}", 23);
-    listener.event(event1);
+    listener.event(event2);
     listener.close();
-    assertEquals("{\"severity\":\"ERROR\",\"message\":\"Error message 23\"}", 
+    assertEquals("{\"events\":[{\"severity\":\"ERROR\",\"message\":\"Error message 23\"}]}", 
+        outputStream.toString());
+  }
+  
+  @Test
+  void multiple() throws Exception {
+    listener.event(event1);
+    listener.event(event2);
+    listener.close();
+    assertEquals("{\"events\":[{\"severity\":\"WARN\",\"message\":\"Warning message\"},{\"severity\":\"ERROR\",\"message\":\"Error message 23\"}]}", 
         outputStream.toString());
   }
 
