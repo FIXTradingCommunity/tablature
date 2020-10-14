336a3884c0e8dee4cb2a8d72f81b9e025d9d13d9
Don Mendelson <donmendelson@gmail.com>
2020-10-14 15:03:11 -0500

    Move orchestra-common module #36

--- orchestra-common/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module orchestra.common {
-  exports io.fixprotocol.orchestra.event;
-  
-  opens io.fixprotocol.orchestra.event;
-  requires com.fasterxml.jackson.core;
-  requires org.apache.logging.log4j;
-}
